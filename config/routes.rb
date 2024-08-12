46ae486e9bda181762db1631787410a5eafa86a5
cami oliva <lamica.olivares@gmail.com>
2024-08-11 21:04:10 -0400

    movie blog rails

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'movies/index'
+  get 'movies/create'
+  get 'movies/new'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+   root "movies#index"
+   post "movies" => "movies#create"
+end
